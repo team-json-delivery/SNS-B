1e88bbf2deaab648cf9de771276c7052894bbf6a
taetaetae <taetaetae_@naver.com>
2025-02-02 21:51:34 +0900

    조회시 없을 경우 예외처리

--- src/main/kotlin/team_json_delivery/sns_b/global/exception/ErrorCode.kt
+++ src/main/kotlin/team_json_delivery/sns_b/global/exception/ErrorCode.kt
@@ -5,4 +5,5 @@ enum class ErrorCode(
     val message: String
 ) {
     NOT_FOUND_POST("NOT_FOUND_POST", "게시글이 존재하지 않습니다."),
+    NOT_FOUND_USER("NOT_FOUND_USER", "사용자가 존재하지 않습니다."),
 }
--- src/main/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandler.kt
@@ -4,13 +4,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import team_json_delivery.sns_b.domain.post.exception.NotFoundPostException
+import team_json_delivery.sns_b.domain.post.exception.NotFoundUserException
 import team_json_delivery.sns_b.global.model.response.WebResponse
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NotFoundPostException::class)
+    @ExceptionHandler(
+        value = [
+            BusinessException::class,
+            NotFoundUserException::class
+        ]
+    )
     fun badRequest(e: BusinessException): WebResponse<Any> {
         return WebResponse.error(e.errorCode)
     }
