5f69b8fac11f74a4c89aee371522845659eb42f3
DESKTOP-DLS1F2V\yoonhc <ychqwertyuio@naver.com>
2025-02-08 15:12:30 +0900

    refactor: ErrorCode 내 status 추가

--- src/main/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandler.kt
@@ -1,30 +1,43 @@
 package team_json_delivery.sns_b.global.exception
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import team_json_delivery.sns_b.domain.post.exception.NotFoundPostException
-import java.lang.Exception
 
 @RestControllerAdvice
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
-    @ExceptionHandler(NotFoundPostException::class)
-    fun badRequest(
-        ex: NotFoundPostException,
+    @ExceptionHandler(BusinessException::class)
+    fun businessExceptionHandler(
+        ex: BusinessException,
         request: WebRequest,
     ): ProblemDetail =
         createProblemDetail(
             ex = ex,
-            status = HttpStatus.BAD_REQUEST,
+            status = ex.errorCode.status,
             defaultDetail = ex.errorCode.message,
             detailMessageCode = ex.errorCode.code,
             detailMessageArguments = null,
             request = request,
         )
+
+    @ExceptionHandler(Exception::class)
+    fun exceptionHandler(
+        ex: RuntimeException,
+        request: WebRequest,
+    ): ProblemDetail {
+        val errorCode = ErrorCode.INTERNAL_SERVER_ERROR
+        return createProblemDetail(
+            ex = ex,
+            status = errorCode.status,
+            defaultDetail = errorCode.message,
+            detailMessageCode = errorCode.code,
+            detailMessageArguments = null,
+            request = request,
+        )
+    }
 
     override fun createProblemDetail(
         ex: Exception,
@@ -45,8 +58,12 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
             )
 
         if (ex is BusinessException) {
-            problemDetail.setProperty("errorCode", ex.errorCode.code)
+            problemDetail.setProperty(ERROR_CODE, ex.errorCode.code)
         }
         return problemDetail
+    }
+
+    companion object {
+        const val ERROR_CODE = "errorCode"
     }
 }
--- src/test/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandlerTest.kt
+++ src/test/kotlin/team_json_delivery/sns_b/global/exception/GlobalExceptionHandlerTest.kt
@@ -6,11 +6,14 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.isEqualTo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team_json_delivery.sns_b.domain.post.exception.NotFoundPostException
+import team_json_delivery.sns_b.global.exception.GlobalExceptionHandler.Companion.ERROR_CODE
 
 @WebMvcTest(GlobalExceptionHandlerTestController::class)
 class GlobalExceptionHandlerTest(
@@ -18,11 +21,23 @@ class GlobalExceptionHandlerTest(
 ) : DescribeSpec({
 
         describe("NotFoundPostException 호출") {
-            it("BadRequest 호출") {
+            it("호출") {
+                val ex = NotFoundPostException()
                 mockMvc
                     .perform(get("/notfoundpost"))
                     .andDo(print())
-                    .andExpect(status().isBadRequest)
+                    .andExpect(status().isEqualTo(ex.errorCode.status.value()))
+                    .andExpect(jsonPath("$.$ERROR_CODE").value(ex.errorCode.code))
+            }
+        }
+
+        describe("RuntimeException 호출") {
+            it("ErrorCode.INTERNAL_SERVER_ERROR 호출") {
+                val errorCode = ErrorCode.INTERNAL_SERVER_ERROR
+                mockMvc
+                    .perform(get("/exception"))
+                    .andDo(print())
+                    .andExpect(status().isEqualTo(errorCode.status.value()))
             }
         }
 
@@ -40,5 +55,8 @@ class GlobalExceptionHandlerTest(
 @RequestMapping
 class GlobalExceptionHandlerTestController {
     @GetMapping("/notfoundpost")
-    fun badRequest(): String = throw NotFoundPostException()
+    fun notfoundpost(): String = throw NotFoundPostException()
+
+    @GetMapping("/exception")
+    fun exception(): String = throw RuntimeException()
 }
